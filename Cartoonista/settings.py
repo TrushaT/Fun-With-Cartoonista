9b9a33502f4d9bf30939cae2554916b991e42985
TrushaT <trusha.talati@spit.ac.in>
2020-10-14 19:15:47 +0530

    "password-reset-issue-resolved"

--- Cartoonista/settings.py
+++ Cartoonista/settings.py
@@ -155,5 +155,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('CARTOONISTA_EMAIL_HOST_PASSWORD')
-EMAIL_HOST_PASSWORD = os.environ.get('CARTOONISTA_EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'hellofromcartoonista@gmail.com'
+EMAIL_HOST_PASSWORD = 'ylgklbdqvwkatnfe'
+
