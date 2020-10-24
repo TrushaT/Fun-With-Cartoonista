b00bbaafdcb601fd04424695771a204481d6b964
TrushaT <trusha.talati@spit.ac.in>
2020-10-24 15:52:33 +0530

    debug value set

--- Cartoonista/settings.py
+++ Cartoonista/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('CARTOONISTA_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['127.0.0.1','funwithcartoonista.herokuapp.com']
 
